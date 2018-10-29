c5d8d611599b7ad90cc9d3df881b436b8d99eb50
Ed <sfuptown@live.com>
2018-10-29 01:47:26 +0000

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hey There. This is my personal Gatsby Site</h1>
+    <p>Gatsby is cool.</p>
+    <p>Now, let's build something great.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
